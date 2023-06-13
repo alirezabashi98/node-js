c4c73fab352541b635f83205713d69f51a159704
Alireza Bashi <alirezabashi98@gmail.com>
2023-06-13 21:22:06 +0330

    Split the Query String & Read the Query String

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
 
+// http://localhost:8080/summer => /summer
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write(req.url);
+//   res.end();
+// }).listen(8080);
+
+var http = require('http');
+var url = require('url');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month + " " + q.day ;
+  res.end(txt);
 }).listen(8080);
+
