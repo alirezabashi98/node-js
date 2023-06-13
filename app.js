5cd56bb42b1d15a4db4a95248034c74942d0df9e
Alireza Bashi <alirezabashi98@gmail.com>
2023-06-13 21:13:33 +0330

    my first module

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
+
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
 }).listen(8080);
