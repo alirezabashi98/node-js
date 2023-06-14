fc104f91f79aacbee0d94ae3c3630c13336441cd
Alireza Bashi <alirezabashi98@gmail.com>
2023-06-14 15:37:05 +0330

    The Built-in URL Module

--- app.js
+++ app.js
@@ -1,82 +1,28 @@
+// var url = require('url');
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// var q = url.parse(adr, true);
+
+// console.log(q.host); //returns 'localhost:8080'
+// console.log(q.pathname); //returns '/default.htm'
+// console.log(q.search); //returns '?year=2017&month=february'
+
+// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log(qdata.month); //returns 'february'
+
 var http = require('http');
+var url = require('url');
 var fs = require('fs');
 
-//  Read Files
-// Create a Node.js file that reads the HTML file, and return the content:
-
-// http.createServer(function (req, res) {
-//   fs.readFile('demofile1.html', function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     return res.end();
-//   });
-// }).listen(8080);
-
-
-
-
-
-// Create Files
-// The File System module has methods for creating new files:
-// fs.appendFile()
-// fs.open()
-// fs.writeFile()
-// The fs.appendFile() method appends specified content to a file. If the file does not exist, the file will be created:
-
-// fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-//   if (err) throw err;
-//   console.log('Saved!');
-// });
-
-// The fs.open() method takes a "flag" as the second argument, if the flag is "w" for "writing", the specified file is opened for writing. If the file does not exist, an empty file is created:
-
-// fs.open('mynewfile2.txt', 'w', function (err, file) {
-//   if (err) throw err;
-//   console.log('Saved!');
-// });
-
-// The fs.writeFile() method replaces the specified file and content if it exists. If the file does not exist, a new file, containing the specified content, will be created:
-
-// fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
-//   if (err) throw err;
-//   console.log('Saved!');
-// });
-
-
-
-
-// Update Files
-// The File System module has methods for updating files:
-// fs.appendFile()
-// fs.writeFile()
-// The fs.appendFile() method appends the specified content at the end of the specified file:
-
-// fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
-//   if (err) throw err;
-//   console.log('Updated!');
-// });
-
-// fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
-//   if (err) throw err;
-//   console.log('Replaced!');
-// });
-
-
-
-
-// Delete Files
-// To delete a file with the File System module,  use the fs.unlink() method.\
-// The fs.unlink() method deletes the specified file:
-
-// fs.unlink('mynewfile2.txt', function (err) {
-//   if (err) throw err;
-//   console.log('File deleted!');
-// });
-
-
-
-// Rename Files
-fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
-  if (err) throw err;
-  console.log('File Renamed!');
-});
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
