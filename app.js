412129eace970e620e3ada386aa5e220cc7b52e9
Alireza Bashi <alirezabashi98@gmail.com>
2023-06-14 16:10:24 +0330

    Save the File

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-// var fs = require('fs');
-// var events = require('events');
-// var eventEmitter = new events.EventEmitter();
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-// var rs = fs.createReadStream('./demofile.txt');
-// rs.on('open', function () {
-//   console.log('The file is open');
-// });
-
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
-
-//Create an event handler:
-var myEventHandler = function () {
-  console.log('I hear a scream!');
-}
-
-//Assign the event handler to an event:
-eventEmitter.on('scream', myEventHandler);
-
-//Fire the 'scream' event:
-eventEmitter.emit('scream');
+http.createServer(function (req, res) {
+  if (req.url == '/fileupload') {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var oldpath = files.filetoupload.filepath;
+      var newpath = '/home/alireza/IdeaProjects/node_js/example/' + files.filetoupload.originalFilename;
+      fs.copyFile(oldpath, newpath, function (err) {
+        if (err) throw err;
+        fs.unlink(oldpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+          res.end();
+        });
+      });
+    });
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+  }
+}).listen(8080);
